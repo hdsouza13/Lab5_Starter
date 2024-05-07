cd2406714224c0b8360f95c99833fbecf13dbb15
Hugo <146862313+hdsouza13@users.noreply.github.com>
2024-05-07 01:52:49 -0700

    Update

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,61 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+// explore.js
+
+window.addEventListener('DOMContentLoaded', init);
+
+let voices = [];
+
+function populateVoiceList() {
+  voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
+}
+
+function speak(event){
+  let textarea = document.querySelector('textarea');
+  let input_text = textarea.value;
+
+  let utterance = new SpeechSynthesisUtterance(input_text);
+  const voiceSelect = document.getElementById("voice-select");
+  const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterance.voice = voices[i];
+    }
+  }
+
+  // open smile when speaking
+  let image = document.querySelector('img')
+  image.setAttribute('src', 'assets/images/smiling-open.png')
+
+  speechSynthesis.speak(utterance);  
+  
+  // back to closed smile when finish speaking
+  utterance.addEventListener("end", (event) => {
+    image.setAttribute('src', 'assets/images/smiling.png');
+  });
+}
+
 function init() {
-  // TODO
+  // populate with list of possible voices
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  // convert to speech
+  let submit_button = document.querySelector('button')
+  submit_button.addEventListener('click', event => speak(event))
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,75 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+function select_horn(event){
+
+  // get the horn selected from the dropdown
+  let horn_type = event.target.value;
+
+  // select the image tag and update "src" with the image path of selected horn
+  let image = document.querySelector('img');
+  let image_path = 'assets/images/' + String(horn_type) + '.svg';
+  image.setAttribute("src", image_path);
+
+  // select the audio tag and update the "src" with the audio path of the selected horn
+  let audio = document.querySelector('audio')
+  let audio_path = 'assets/audio/' + String(horn_type) + '.mp3';
+  audio.setAttribute("src", audio_path)
+}
+
+function select_volume(event){
+  // volume selected
+  let volume_level = event.target.value; // between 0 and 100
+  let volume_level_normalized = volume_level / 100; // between 0 and 1
+
+  // seleting the appropriate icon
+  let volume_type = -1;
+  if (volume_level == 0){
+    volume_type = 0;
+  }
+  else if (volume_level < 33){
+    volume_type = 1;
+  }
+  else if (volume_level < 67){
+    volume_type = 2;
+  }
+  else{
+    volume_type = 3;
+  }
+
+  let path = 'assets/icons/volume-level-' + volume_type + '.svg';
+
+  // set the correct volume icon
+  let div_volume_control = document.getElementById('volume-controls');
+  let volume_image = div_volume_control.querySelector('img');
+  volume_image.setAttribute("src", path);
+
+  // set the volume
+  let audio = document.querySelector('audio');
+  audio.volume = volume_level_normalized;
+}
+
+function play_audio(){
+  let audio = document.querySelector('audio');
+  audio.play();
+
+
+  // add confetti if the party horn is selectedf
+  if (audio.getAttribute('src') == 'assets/audio/party-horn.mp3'){
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
+  }
+  
+}
+
 function init() {
-  // TODO
+  
+  let dropdown = document.querySelector('select');
+  dropdown.addEventListener('change', event => select_horn(event));
+
+  let volume = document.getElementById('volume');
+  volume.addEventListener('change', event => select_volume(event));
+
+  let play_button  = document.querySelector('button');
+  play_button.addEventListener('click', play_audio);
 }
