cf0edc9d9e1f4ec9024f0f7a365ef2bb6ee35291
Hugo <146862313+hdsouza13@users.noreply.github.com>
2024-05-07 20:07:43 -0700

    Update

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,23 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+//false
+test('adds 15 + 15 to equal 20', () => {
+  expect(15 + 15).toBe(20);
+});
+
+//false
+test('adds 15 and 20 equal to each other', () => {
+  expect(15 == 20).toBe(true);
+});
+
+//true
+test('adds 15 and 15 equal to each other', () => {
+  expect(15 == 15).toBe(true);
+});
+
+//true
+test('50 is greater than 100 when doing string comparison', () => {
+  expect("50" > "100").toBe(true);
+});
